4e99a7cf05a881d6a1014429181aaaee728e6e91
JamesMReilly <jimreilly28@gmail.com>
2018-04-07 18:12:31 -0500

    Created plot of daily natural gas consumption data over time

---
+++ js/script.js
@@ -0,0 +1,93 @@
+var svg = d3.select("#flow");
+
+var width = svg.attr("width");
+var height = svg.attr("height");
+
+//paddings for minimized size of graph to fit labels/title
+var xPadding = 80;
+var yPadding = 80;
+
+// Define variables outside the scope of the callback function.
+var gasData;
+var dateExtent;
+var flowExtent;
+
+var dateScale;
+var flowScale;
+
+// This function will be applied to all rows of GasData.csv
+function parseLine(line) {
+  return {
+    Date: Date.parse(line["Date"]),
+    Temp: parseFloat(line["Temp"]),
+    WindSpeed: parseFloat(line["Wind Speed"]),
+    Flow: parseFloat(line["Flow"])
+  };
+}
+
+d3.csv("data/GasData.csv", parseLine, function (error, data) {
+  console.log(error);
+  gasData = data;
+
+  console.log(gasData);
+  dateExtent = d3.extent(gasData, function (d) { return d.Date; });
+  flowExtent = d3.extent(gasData, function (d) { return d.Flow; });
+
+  console.log(dateExtent);
+  console.log(flowExtent);
+
+  update();
+});
+
+function update() {
+  //remove all content from svg
+  svg.selectAll("g > *").remove();
+  svg.selectAll("text").remove();
+  svg.selectAll("circle").remove();
+
+  dateScale = d3.scaleLinear()
+    .domain(dateExtent)
+    .range([xPadding, width - xPadding])
+
+  flowScale = d3.scaleLinear()
+    .domain(flowExtent)
+    .range([height - yPadding, yPadding])
+
+  plotGrowthOverInitialOverall(svg, gasData, dateScale, flowScale)
+}
+
+function plotGrowthOverInitialOverall(svg, gasData, dateScale, flowScale) {
+  gasData.map(function (dayWithData) {
+    svg.append("circle")
+      .attr("cx", dateScale(dayWithData.Date))
+      .attr("cy", flowScale(dayWithData.Flow))
+      .attr("r", 2)
+      .style("fill", "#45b3e7")
+  })
+
+  //x-axis, current overall rating
+  var bottomAxis = d3.axisBottom(dateScale)
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - xPadding) + ")")
+    .attr("class", "xaxis")
+    .call(bottomAxis);
+
+  //y-axis, growth in overall
+  var leftAxis = d3.axisLeft(flowScale);
+  svg.append("g")
+    .attr("class", "yaxis")
+    .attr("transform", "translate(" + yPadding + ", 0)")
+    .call(leftAxis);
+
+  //x-axis label
+  svg.append("text")
+    .attr("transform", "translate(" + (width / 2.3) + "," + (height - (xPadding / 2)) + ")")
+    .text("Date");
+
+  //y-axis label, rotated to be vertical text
+  svg.append("text")
+    .attr("transform", "translate(" + yPadding / 3 + "," + (height / 1.7) + ")rotate(270)")
+    .text("Scaled Sendout");
+
+}
+
